2a9e8464b85d3cf1daeeddefc2f46abb4df5beb3
ThePowerdinoDeluxe990 <toniservera990@gmail.com>
2025-09-03 10:26:48 +0200

    Pretty MapCard.kt

--- app/src/main/java/com/powerdino/splatoon3_companion/ui/composables/MapCard.kt
+++ app/src/main/java/com/powerdino/splatoon3_companion/ui/composables/MapCard.kt
@@ -1,16 +1,23 @@
 package com.powerdino.splatoon3_companion.ui.composables
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.powerdino.splatoon3_companion.R
 
 @Composable
 fun MapCard(
@@ -23,17 +30,43 @@ fun MapCard(
         ),
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surfaceVariant,
-        ),
-        modifier = Modifier
-            .size(width = 180.dp,height = 100.dp)
+        )
     ){
         Column{
-            Text(mapName)
             Image(
                 painter = painterResource(mapImage),
-                contentDescription = mapName
+                contentDescription = mapName,
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop
             )
+            Text(
+                text=mapName,
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(12.dp)
+            )
+
         }
 
     }
+}
+@Preview(
+    showBackground = true,
+    name = "Light",
+    showSystemUi = true,
+    backgroundColor = 0xFFFCFCFF
+)
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "Dark",
+    showSystemUi = true,
+    backgroundColor = 0xFF1A1C1E
+)
+@Composable
+fun PreviewMapCard(){
+    MapCard(
+        mapName= stringResource(R.string.stage1),
+        mapImage = R.drawable.stage1
+    )
 }
--- app/src/main/java/com/powerdino/splatoon3_companion/ui/screens/routes/RegularBattlesScreen.kt
+++ app/src/main/java/com/powerdino/splatoon3_companion/ui/screens/routes/RegularBattlesScreen.kt
@@ -1,11 +1,16 @@
 package com.powerdino.splatoon3_companion.ui.screens.routes
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.powerdino.splatoon3_companion.data.listOfMpMaps
 import com.powerdino.splatoon3_companion.model.Data
 import com.powerdino.splatoon3_companion.ui.composables.MapCard
@@ -20,9 +25,17 @@ fun RegularBattlesScreen (
             items(splatoonNormal.normal){ items ->
                 Text(items.regular.rule)
                 items.regular.stages.forEach {it ->
-                    MapCard(
-                        mapName = stringResource(listOfMpMaps[it-1].nameState),
-                        mapImage = listOfMpMaps[it-1].imageState
+                    Box(
+                        modifier = Modifier.padding(horizontal = 8.dp)
+                    ){
+                        MapCard(
+                            mapName = stringResource(listOfMpMaps[it-1].nameState),
+                            mapImage = listOfMpMaps[it-1].imageState
+                        )
+
+                    }
+                    Spacer(
+                        modifier = Modifier.padding(12.dp)
                     )
                 }
             }
