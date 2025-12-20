45eb3fb52bde6825fc1b69f8297691d990a7955c
ThePowerdinoDeluxe990 <118630450+ThePowerdinoDeluxe990@users.noreply.github.com>
2025-12-20 21:57:28 +0100

    Changed versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 8
-        versionName = "0.9.7.0"
+        versionName = "0.9.7.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
