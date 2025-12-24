54435ca912191af6613adc2ef4633759500e376f
ThePowerdinoDeluxe990 <118630450+ThePowerdinoDeluxe990@users.noreply.github.com>
2025-12-24 16:23:07 +0100

    TODO: Finish tests

--- app/src/test/java/com/powerdino/splatoon3_companion/fake/FakeApiService.kt
+++ app/src/test/java/com/powerdino/splatoon3_companion/fake/FakeApiService.kt
@@ -1,10 +1,20 @@
 package com.powerdino.splatoon3_companion.fake
 
 import com.powerdino.splatoon3_companion.model.Data
+import com.powerdino.splatoon3_companion.model.salmon_run.Salmon
+import com.powerdino.splatoon3_companion.model.salmon_run.resources.SalmonResources
 import com.powerdino.splatoon3_companion.service.RetroFitService
 
 class FakeApiService : RetroFitService {
     override suspend fun getData(): Data {
         return FakeDataSource.getSplatoonData()
+    }
+
+    override suspend fun getSalmonResources(): SalmonResources {
+
+    }
+
+    override suspend fun getSalmonSchedules(): Salmon {
+
     }
 }
--- app/src/test/java/com/powerdino/splatoon3_companion/fake/FakeDataSource.kt
+++ app/src/test/java/com/powerdino/splatoon3_companion/fake/FakeDataSource.kt
@@ -8,6 +8,9 @@ import com.powerdino.splatoon3_companion.model.League
 import com.powerdino.splatoon3_companion.model.Normal
 import com.powerdino.splatoon3_companion.model.Regular
 import com.powerdino.splatoon3_companion.model.X
+import com.powerdino.splatoon3_companion.model.salmon_run.resources.Enemy
+import com.powerdino.splatoon3_companion.model.salmon_run.resources.Events
+import com.powerdino.splatoon3_companion.model.salmon_run.resources.SalmonResources
 
 object FakeDataSource {
     fun getSplatoonData(): Data{
@@ -52,6 +55,56 @@ object FakeDataSource {
                 ),
             ),
             fest = Fest
+        )
+    }
+
+    fun getSalmonResources(): SalmonResources{
+        return SalmonResources(
+            enemy = Enemy(
+                sakediver = "",
+                sakedozer = "",
+                sakeArtillery = " ",
+                sakeBigMouth = " ",
+                sakeBigMouthGold = " ",
+                sakeCopter = " ",
+                sakeDolphin = " ",
+                sakeFlyBagman = " ",
+                sakeJaw = " ",
+                sakePillar = " ",
+                sakeRope = " ",
+                sakeSaucer = " ",
+                sakelienBomber = " ",
+                sakelienCupTwins = " ",
+                sakelienGiant = " ",
+                sakelienGolden = " ",
+                sakelienLarge = " ",
+                sakelienShield = " ",
+                sakelienSmall = " ",
+                sakelienSnake = " ",
+                sakelienStandard = " ",
+                sakelienTower = " ",
+                sakerocket = " ",
+                triple = " ",
+            ),
+            events = Events(
+                dozer = " ",
+                fog = " ",
+                geyser = " ",
+                hakobiya = " ",
+                missile = " ",
+                relay = " ",
+                rush = " ",
+                tamaire = " "
+            ),
+            gearclothes ="" ,
+            gearhead = " ",
+            gearshoes = " ",
+            locker = " ",
+            modes = " ",
+            stages = " ",
+            tides = " ",
+            weaponsmain = " ",
+            weaponsspecial = " "
         )
     }
 }
